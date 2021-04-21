8be19788183df95a39bd02c686cb67b58862a17a
shimotaroo <shimotaro.0327@gmail.com>
2021-04-21 15:10:21 +0900

    validation機能の追加

--- pages/register.vue
+++ pages/register.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="m-4 p-4 bg-white shadow rounded">
     <h2 class="text-2xl text-center text-darkGray">アカウント登録</h2>
-    <form>
+    <form @submit.prevent="onSubmit">
       <div class="flex items-center justify-center flex-col w-full h-full mt-8">
+        <!-- form.imageUrl.errorMessage が null ではなかった場合、class に左側の border-red-500 を追加 -->
+        <!-- v-bind:class に {適用したいclass: 条件} という Object を渡すと class を動的に切り替えることが出来る -->
         <div
+          :class="{ 'border-red-500': form.imageUrl.errorMessage }"
           class="flex items-center justify-center w-32 h-32 bg-gray-200 rounded-full border border-gray-400 "
         >
-        <template v-if="form.imageUrl.val">
+          <template v-if="form.imageUrl.val">
             <img
               :src="form.imageUrl.val"
               class="w-32 h-32 object-cover border rounded-full"
@@ -27,6 +30,9 @@
             @change="onSelectFile"
           />
         </div>
+        <span v-show="form.imageUrl.errorMessage" class="text-red text-sm">
+          {{ form.imageUrl.errorMessage }}
+        </span>
       </div>
       <label
         class="block mt-8 mb-2 ml-2 uppercase tracking-wide text-darkGray text-s"
@@ -34,10 +40,17 @@
         名前
       </label>
       <div class="h-20 mb-6">
+        <!-- @blurはフォーカスが外れた時に発火するイベント -->
         <input
+          v-model="form.name.val"
+          :class="{ 'border-red-500': form.name.errorMessage }"
           type="text"
           class="block w-full py-3 px-4 appearance-none bg-gray-200 text-darkGray border rounded leading-tight focus:outline-none focus:bg-white"
+          @blur="validateName"
         />
+        <span v-show="form.name.errorMessage" class="text-red text-sm">
+          {{ form.name.errorMessage }}
+        </span>
       </div>
 
       <div class="flex">
@@ -57,8 +70,15 @@ export default {
   data () {
     return {
       form: {
+        name: {
+          label: '名前',
+          val: null,
+          errorMessage: null
+        },
         imageUrl: {
-          val: null
+          label: 'アイコン画像',
+          val: null,
+          errorMessage: null
         }
       }
     }
@@ -104,6 +124,40 @@ export default {
       // ファイルを適用してファイルアップロードを開始
       const snapShot = await imageRef.put(localImageFile)
       this.form.imageUrl.val = await snapShot.ref.getDownloadURL()
+
+      this.validateImageUrl()
+    },
+    // バリデーション（名前）
+    validateName () {
+      const { name } = this.form
+      const maxLength = 8
+
+      if (!name.val) {
+        name.errorMessage = `${name.label}は必須入力項目です`
+        return
+      }
+
+      if (name.val.length > maxLength) {
+        name.errorMessage = `${name.label}は${maxLength}文字以内で入力してください`
+        return
+      }
+
+      name.errorMessage = null
+    },
+    // バリデーション（アイコン画像）
+    validateImageUrl () {
+      const { imageUrl } = this.form
+
+      if (!imageUrl.val) {
+        imageUrl.errorMessage = `${imageUrl.label}は必須入力項目です`
+        return
+      }
+
+      imageUrl.errorMessage = null
+    },
+    onSubmit () {
+      this.validateName()
+      this.validateImageUrl()
     }
   }
 }
